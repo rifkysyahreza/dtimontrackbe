a04535435d2ded74ff9c24be93a42ca81f9cf5d8
Rifky Syahreza <rsyahrezafs@gmail.com>
2024-11-01 13:48:11 +0700

    feat: initial commit

---
+++ src/main/java/com/dti/montrackbe/MontrackbeApplication.java
@@ -0,0 +1,13 @@
+package com.dti.montrackbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontrackbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontrackbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dti/montrackbe/MontrackbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dti.montrackbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontrackbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
